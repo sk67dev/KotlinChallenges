10badda64ab7662a1c1e0706d7bab2d45696f14f
Sakari De-Meis <sakaridemeis@gmail.com>
2019-08-23 01:23:39 +0200

    Add NameChange challenge

---
+++ src/NameChange.kt
@@ -0,0 +1,20 @@
+fun main() {
+}
+//Constructor function
+class NameChange(_oldName: String, _newName: String) {
+    //Properties
+    val oldName: String
+    val newName: String
+
+    //Initializer block
+    init {
+        this.oldName = _oldName
+        this.newName = _newName
+
+        //Print both the old and the new name.
+        println("Old name was $_oldName \nNew name is $_newName.")
+    }
+}
+//Object instances
+val person1 = NameChange("Sally Meadows", "Sally Winterbottom")
+
