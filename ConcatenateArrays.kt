528f871c9517c1f88a344df3f4486354435561ba
Sakari Bansal <43090937+SK1dev@users.noreply.github.com>
2019-08-23 13:32:16 +0200

    Add files via upload

---
+++ ConcatenateArrays.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() {
+
+    val array1 = intArrayOf(1,2,3)
+    val array2 = intArrayOf(4,5,6)
+
+    val aLen = array1.size
+    val bLen = array2.size
+    val result = IntArray(aLen + bLen)
+
+    System.arraycopy(array1, 0, result, 0, aLen)
+    System.arraycopy(array2, 0, result, aLen, bLen)
+
+    println(Arrays.toString((result)))
+}
