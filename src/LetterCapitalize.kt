8a3772989cb2d626ee1eccb22151f72c3898314d
Sakari Bansal <43090937+SK1dev@users.noreply.github.com>
2019-08-23 20:28:06 +0200

    Update LetterCapitalize.kt

--- src/LetterCapitalize.kt
+++ src/LetterCapitalize.kt
@@ -1,7 +1,7 @@
 fun main() {
 
     fun String.capitalizeWords(): String = split(" ").map { it.capitalize() }
-        .joinToString()
+        .joinToString(" ")
     val str = "These are the capitalized words."
     println(str.capitalizeWords())
 }
